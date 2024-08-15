84fb35733be5e9e78dbc36c97eb1c648dc84e89d
RayBirbz <ray.wong1@education.nsw.gov.au>
2024-08-16 09:58:40 +1000

    Changes to main.py 16/08/2024
    There were 2 new changes to this commit. They included:
    - 3 New functions (To show the original and updated dataset, as well as an average of selling price function)
    - Setup of the basic user interface
    There were a few issues:
    - The option to view the average selling price was giving errors. It was because the column was specified as 'Owner', which didn't exist in the dataset
    - The User Interface was spammed in the terminal, but was resolved by adding the actual options, because the first prototype of the user interface only included the print() text.
    - Attempting to drop the column 'Owner' (because it wasn't necessary), kept failing. It was resolved by adding 'axis=1'.
    Conclusion:
    Not all the code worked as intended, mainly because of some aspects of code being missing (e.g. 'axis=1'. There was also a lot of problems trying to install pip and pandas into this machine (its not a school computer), which might explain why a file 'get-pip.py' is now added to the program.
    
    Next, I will add some more analysis options.

--- Datasets/main.py
+++ Datasets/main.py
@@ -1,6 +1,13 @@
 # Modules
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
+
+
+# -- Variables --
+
+# Quit
+quit = False
 
 # -- Dataframe Setup --
 
@@ -8,10 +15,13 @@ import matplotlib.pyplot as plt
 original = pd.read_csv('Datasets/data/cardata.csv')
 
 # Setting up the new dataframe and its organised version
-new = pd.read_csv('Datasets/data/cardata.csv',
-                  header=0,
-                  names = ['Car Name', 'Year', 'Selling Price', 'Present Price', 'Kilometres Driven', 'Fuel Type', 'Seller Type'])
-print(new)
+new = pd.read_csv('Datasets/data/cardata.csv')
+new = new.drop(columns=['Owner'], axis=1)
+new.head()
+
+# -- Additional Variables --
+
+# Average selling price
 
 # -- Functions --
 
@@ -22,4 +32,41 @@ def ShowOriginal():
 # Showing the updated dataframe
 def ShowNew():
     print(new)
-    
+# Showing the average 
+def avg_new():
+    average_new = new['Selling_Price'].mean()
+    print(average_new)
+
+# The user interface and options
+
+def Options():
+    global quit
+    print("""To analyse the dataset of Cars, please select one of the options below: 
+          
+          1 - Show the original dataset
+          2 - Show the new and updated dataset
+          3 - View the average selling price for all the cars
+          4 - View the average present price for all the cars
+          5 - View the general info of the dataset
+          6 - Quit
+          """)
+    try:
+        option = int(input('Enter your selection: '))
+        if option == 1:
+            ShowOriginal()
+        elif option == 2:
+            ShowNew()
+        elif option == 3:
+            avg_new()
+        elif option == 5:
+            new.info()
+        elif option == 6:
+            quit = True
+        else:
+            print("Please select a valid choice.")
+
+    except:
+        print("Please enter an option in the appropiate format.")
+
+while not quit:
+    Options()
