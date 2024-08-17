ac0dd3e66cf6b62084929625887c07bcfef43b65
RayBirbz <ray.wong1@education.nsw.gov.au>
2024-08-17 16:57:40 +1000

    Updated main.py 17/08/2024
    3 new changes included:
    - **REVAMP** of the entire UI. It is a try-except loop with try-except loops inside of it. This is to make way for a new menu
    - 2 new functions: Options and more_options. An original options menu and additional options, which creates a more organised menu purely for analysis details (e.g. mean)
    - Updated selling and current price functions by adding a print statement to make it more neat and user-friendly.
    Issues and bugs:
    - The main issue was trying to exit the program. I tried multiple different methods, including quit, sys.exit (which made the program not restartable), break (which was too weak). All of the methods didn't work out for the UI.
    Instead, I used the return function, which actually worked by exiting the whole function,
    Removals:
    removed the loop for the UI. It still works without the loop.
    Further explanation of the revamp:
    The new UI function tracks which of the options should be shown by using a true/false variable.  Depending on its state, it switches to either the original options or the analysis options. This makes the UI more organised and can sort different actions into their own categories. If an error occurs, it will quit the entire program (sys.exit).
    What next?
    I will add some data visualisation options, as well as more general analysis options. Also need to make sure the UI continuously runs.

--- Datasets/main.py
+++ Datasets/main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+import sys
 
 
 # -- Variables --
@@ -22,6 +23,13 @@ new.head()
 # -- Additional Variables --
 
 # Average selling price
+def avg_new():
+    average_new = new['Selling_Price'].mean()
+    print(f'The average selling price is '[average_new])
+# AVerage currrent price
+def avg_new_present():
+    average_new_present = new['Present_Price'].mean()
+    print(f'The average present price is '[average_new_present])
 
 # -- Functions --
 
@@ -32,41 +40,75 @@ def ShowOriginal():
 # Showing the updated dataframe
 def ShowNew():
     print(new)
-# Showing the average 
-def avg_new():
-    average_new = new['Selling_Price'].mean()
-    print(average_new)
 
-# The user interface and options
+
+# The user interface 
+
+def UI():
+    try:
+        main_options = True
+
+        while True: # The variables and IF statements that keep track of which options to show
+            if main_options:
+                Options()
+            else:
+                more_options()
+            if main_options: # The main opptions
+                try:
+                    option = int(input('Enter your selection: '))
+                    if option == 1:
+                        ShowOriginal()
+                    elif option == 2:
+                        ShowNew()
+                    elif option == 3:
+                        main_options = False
+                    elif option == 4:
+                        new.info()
+                    elif option == 5:
+                        print('Quitting...')
+                        time.sleep(500)
+                        return
+                    else:
+                        print("Please select a valid choice.")
+
+                except:
+                    print("Please enter an option in the appropiate format.")
+            else: # The analysis options 
+                try:
+                    option = int(input('Enter your selection: '))
+                    if option == 1:
+                        avg_new()
+                    elif option ==  2:
+                        avg_new_present()
+                    elif option == 3:
+                        main_options = True
+                    else:
+                        print('Please select a valid choice')
+                except:
+                        print("Please enter an option in the appropiate format.")
+    except:
+        print('Error')
+        sys.exit()
+
+# Main Options
 
 def Options():
-    global quit
     print("""To analyse the dataset of Cars, please select one of the options below: 
           
           1 - Show the original dataset
           2 - Show the new and updated dataset
-          3 - View the average selling price for all the cars
-          4 - View the average present price for all the cars
-          5 - View the general info of the dataset
-          6 - Quit
+          3 - View more analysis options
+          4 - View the general info of the dataset
+          5 - Quit
           """)
-    try:
-        option = int(input('Enter your selection: '))
-        if option == 1:
-            ShowOriginal()
-        elif option == 2:
-            ShowNew()
-        elif option == 3:
-            avg_new()
-        elif option == 5:
-            new.info()
-        elif option == 6:
-            quit = True
-        else:
-            print("Please select a valid choice.")
 
-    except:
-        print("Please enter an option in the appropiate format.")
-
-while not quit:
-    Options()
+# More options for analysis
+def more_options():
+    print("""To analyse additional information on the dataset of Cars, please select one of the options below: 
+          
+          1 - Show the average selling price
+          2 - Show the average present price
+          3 - Go back
+          """)
+# Running the UI
+UI()
