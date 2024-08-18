29b38fbfd15b6dc0f1fb47960ace9dee18c02485
RayBirbz <ray.wong1@education.nsw.gov.au>
2024-08-18 14:51:29 +1000

    Updated main.py 18/08/2024
    New changes:
    - 3 new visualisation options (viewing selling and present prices over the years, and the relationship between the present prices and the transmission)
    - 2 new text-based analysis options (viewing the most and least expensive cars, and which transmission appears the most)
    - Converted LAKH INR to AUD (for those looking to see the Australian values)
    Issues and bugs:
    Sometimes couldn't view the graphs for some reason. Couldn't explain why, but could be resolved by creating completely new functions.
    What next?
    Need to test! All analysis options are complete.

--- Datasets/main.py
+++ Datasets/main.py
@@ -10,6 +10,9 @@ import sys
 # Quit
 quit = False
 
+# Conversion to AUD rate
+conversion_rate = 100000 * 0.018
+
 # -- Dataframe Setup --
 
 # Setting up the original dataframe 
@@ -20,19 +23,41 @@ new = pd.read_csv('Datasets/data/cardata.csv')
 new = new.drop(columns=['Owner'], axis=1)
 new.head()
 
-# -- Additional Variables --
+# -- Additional Functions --
 
 # Average selling price
 def avg_new():
     average_new = new['Selling_Price'].mean()
-    print(f'The average selling price is '[average_new])
+    average_new_aud = conversion_rate * average_new
+    print(average_new_aud)
 # AVerage currrent price
 def avg_new_present():
     average_new_present = new['Present_Price'].mean()
-    print(f'The average present price is '[average_new_present])
+    average_new_present_aud = conversion_rate * average_new_present
+    print(average_new_present_aud)
 
 # -- Functions --
 
+# Checking the most and least expensive car in the list
+def most_least_expensive():
+    most_expensive_sell = new.loc[new['Selling_Price'].idxmax()]
+    least_expensive_sell = new.loc[new['Selling_Price'].idxmin()]
+    most_expensive_present = new.loc[new['Present_Price'].idxmax()]
+    least_expensive_present = new.loc[new['Present_Price'].idxmin()]
+    print('Most expensive car for its selling price:')
+    print(most_expensive_sell['Car_Name'])
+    print('Most expensive car for its present price:')
+    print(most_expensive_present['Car_Name'])
+    print('Least expensive car for its selling price:')
+    print(least_expensive_sell['Car_Name'])
+    print('Least expensive car for its present price:')
+    print(least_expensive_present['Car_Name'])
+# Calculating if there are more automatic or manual cars
+
+def frequent_transmission():
+    transmmission_count_print = new['Transmission'].value_counts()
+    print("Here are the quantities of manual and automatic cars in the dataset:")
+    print(transmmission_count_print)
 # Showing the original dataframe
 def ShowOriginal():
     print(original)
@@ -41,6 +66,39 @@ def ShowOriginal():
 def ShowNew():
     print(new)
 
+# Showing the kilometres driven by the cars
+def sellingpriceevo():
+    new.plot(
+        kind = 'scatter',
+        x = 'Year',
+        y = 'Selling_Price',
+        color = 'blue',
+        alpha = 0.3,
+        title = 'Selling prices over the years'
+    )
+    plt.show()
+
+def presentpriceevo():
+    new.plot(
+        kind = 'scatter',
+        x = 'Year',
+        y = 'Present_Price',
+        color = 'green',
+        alpha = 0.3,
+        title = 'Present prices over the years'
+    )
+    plt.show()
+
+def transmission_presentprice():
+    new.plot(
+        kind = 'scatter',
+        x = 'Transmission',
+        y = 'Present_Price',
+        color = 'red',
+        alpha = 0.3,
+        title = 'Relationship between transmission and selling price'
+    )
+    plt.show()
 
 # The user interface 
 
@@ -81,6 +139,16 @@ def UI():
                     elif option ==  2:
                         avg_new_present()
                     elif option == 3:
+                        frequent_transmission()
+                    elif option == 4:
+                        most_least_expensive()
+                    elif option == 5:
+                        sellingpriceevo()
+                    elif option == 6:
+                        presentpriceevo()
+                    elif option == 7:
+                        transmission_presentprice()
+                    elif option == 8:
                         main_options = True
                     else:
                         print('Please select a valid choice')
@@ -106,9 +174,14 @@ def Options():
 def more_options():
     print("""To analyse additional information on the dataset of Cars, please select one of the options below: 
           
-          1 - Show the average selling price
-          2 - Show the average present price
-          3 - Go back
+          1 - Show the average selling price (in AUD)
+          2 - Show the average present price (in AUD)
+          3 - Show whether there are more automatic or manual cars in the dataset
+          4 - Show the least and most expensive car in the list
+          5 - Visualise the selling price over the years (scatter graph)
+          6 - Visualise the present price over the years (scatter graph)
+          7 - Visualise the relationship between transmission and selling price (scatter graph)
+          8 - Go back
           """)
 # Running the UI
 UI()
